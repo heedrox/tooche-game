955f149e128ed3eadeabfa3546d378e29f8e886a
jmarti <jmarti@theinit.com>
2018-02-25 15:30:55 +0100

    add the work flow

--- src/states/Game.js
+++ src/states/Game.js
@@ -12,6 +12,17 @@ const getRatio = (image, game) => {
   }
   return game.width / image.texture.width;
 };
+const getTargetBoundaries = level => ({
+  left: level.centerPressX - (level.size / 2),
+  right: level.centerPressX + (level.size / 2),
+  top: level.centerPressY - (level.size / 2),
+  bottom: level.centerPressY + (level.size / 2),
+});
+const isWithinBoundaries = (x, y, boundaries, ratio) => x >= (boundaries.left * ratio)
+  && (x <= boundaries.right * ratio)
+  && (y >= boundaries.top * ratio)
+  && (y <= boundaries.bottom * ratio);
+
 export default class extends Phaser.State {
   constructor(isGuiado) {
     super();
@@ -19,21 +30,18 @@ export default class extends Phaser.State {
   }
   init() {
     this.backgroundImage = null;
-    this.currentLevel = LEVELS[0];
+    this.numLevel = 0;
+    this.currentLevel = LEVELS[this.numLevel];
+    this.levelText = null;
+    this.leftArrowControl = null;
+    this.rightArrowControl = null;
   }
 
   create() {
     this.game.scale.scaleMode = Phaser.ScaleManager.RESIZE;
     this.game.scale.parentIsWindow = true;
-    this.addBackgroundImage(this.currentLevel);
-    if (!this.isGuiado) {
-      this.addTarget(this.currentLevel);
-    }
-    if (this.isGuiado) {
-      this.backgroundImage.inputEnabled = true;
-      this.backgroundImage.events.onInputDown.add(this.guiadoClick.bind(this), this);
-    }
-    this.positionElements();
+    this.startTimer();
+    this.paintLevel();
     this.game.scale.onOrientationChange.add((x) => {
       setTimeout(() => {
         console.log('orientation change', x);
@@ -43,17 +51,65 @@ export default class extends Phaser.State {
     });
   }
 
+  paintLevel() {
+    this.addBackgroundImage(this.currentLevel);
+    if (!this.isGuiado) {
+      this.addTarget(this.currentLevel);
+    }
+    this.addLevelText();
+    this.addArrowControls();
+    this.addTimerText();
+    this.positionElements();
+    if (this.numLevel === 0) {
+      this.leftArrowControl.visible = false;
+    } else {
+      this.leftArrowControl.visible = true;
+    }
+  }
+
+  addLevelText() {
+    if (!this.levelText) {
+      this.levelText = this.add.text(50, this.game.height - 100, this.numLevel + 1, { font: '32px Bangers', fill: '#000', align: 'left' });
+    }
+    this.updateText();
+  }
+
+  addTimerText() {
+    if (this.isGuiado) {
+      if (!this.timerText) {
+        this.timerText = this.add.text(this.game.width - 100, this.game.height - 100, '', { font: '32px Bangers', fill: '#000', align: 'left' });
+      }
+    }
+  }
+  updateText() {
+    this.levelText.text = this.numLevel + 1;
+  }
+
+  addArrowControls() {
+    if (!this.leftArrowControl) {
+      this.leftArrowControl = this.add.text(25, 25, '⬅️', { font: '32px Bangers', fill: '#000', align: 'left' });
+      this.leftArrowControl.inputEnabled = true;
+      this.leftArrowControl.events.onInputDown.add(this.previousLevel.bind(this), this);
+    }
+    if (!this.rightArrowControl) {
+      this.rightArrowControl = this.add.text(this.game.width - 25, 75, '➡️', { font: '32px Bangers', fill: '#000', align: 'left' });
+      this.rightArrowControl.inputEnabled = true;
+      this.rightArrowControl.events.onInputDown.add(this.nextLevel.bind(this), this);
+    }
+  }
+
   addBackgroundImage(level) {
+    if (this.backgroundImage) this.backgroundImage.destroy();
     this.backgroundImage = new BackgroundImage({
       game: this.game,
       asset: `image-${level.world}-${level.level}`,
     });
-
     this.game.add.existing(this.backgroundImage);
   }
 
   addTarget(level) {
     const size = level.size;
+    if (this.targetImage) this.targetImage.destroy();
     this.targetImage = this.game.add.sprite(-size / 2, -size / 2, 'target');
     this.targetImage.animations.add('showTarget');
     this.targetImage.animations.play('showTarget', 16, true);
@@ -90,6 +146,24 @@ export default class extends Phaser.State {
       this.targetImage.centerX = this.backgroundImage.left + (this.currentLevel.centerX * ratio);
       this.targetImage.centerY = this.backgroundImage.top + (this.currentLevel.centerY * ratio);
     }
+    if (this.levelText) {
+      this.levelText.x = 25;
+      this.levelText.y = this.game.height - 100;
+    }
+    if (this.leftArrowControl) {
+      this.leftArrowControl.x = 25;
+      this.leftArrowControl.y = 25;
+    }
+    if (this.rightArrowControl) {
+      this.rightArrowControl.x = this.game.width - 50;
+      this.rightArrowControl.y = 25;
+    }
+
+    if (this.isGuiado) {
+      this.backgroundImage.inputEnabled = true;
+      this.backgroundImage.events
+        .onInputDown.add(this.guiadoClick.bind(this), this.backgroundImage);
+    }
   }
 
   render() {
@@ -99,6 +173,7 @@ export default class extends Phaser.State {
   }
 
   update() {
+    this.paintTimer();
     // console.log('positions', this.getWorldPointFromPixelPoint(this.mousePosWorld));
     // this.game.camera.scale = this.game.camera.scale + 0.1;
     // console.log('update', fps);
@@ -111,9 +186,60 @@ export default class extends Phaser.State {
   }
 
   guiadoClick(image, pointer) {
-    const relativeX = pointer.x - image.left;
-    console.log(relativeX);
-    console.log('image current width', image.width);
-    console.log('image original width', image.texture.width);
+    const ratio = getRatio(this.backgroundImage, this.game);
+    const relativeX = pointer.clientX - image.left;
+    const relativeY = pointer.clientY - image.top;
+    const boundaries = getTargetBoundaries(this.currentLevel);
+    if (isWithinBoundaries(relativeX, relativeY, boundaries, ratio)) {
+      alert('YES!');
+      this.nextLevel();
+      if (this.numLevel >= LEVELS.length) {
+        alert('¡FIN! ¡ENHORABUENA!');
+      }
+    } else {
+      alert('WRONG!');
+      this.restart();
+    }
+  }
+
+  previousLevel() {
+    this.numLevel = this.numLevel - 1;
+    this.currentLevel = LEVELS[this.numLevel];
+    this.paintLevel();
+  }
+
+  nextLevel() {
+    this.numLevel = this.numLevel + 1;
+    this.currentLevel = LEVELS[this.numLevel];
+    if (typeof this.currentLevel !== 'undefined') {
+      this.paintLevel();
+    }
+  }
+
+  restart() {
+    this.numLevel = 0;
+    this.currentLevel = LEVELS[this.numLevel];
+    this.paintLevel();
+  }
+
+  startTimer() {
+    if (this.isGuiado) {
+      this.timer = this.game.time.create(false);
+      this.timer.loop(300000, this.endTimer.bind(this), this);
+      this.timer.start();
+    }
+  }
+
+  endTimer() {
+    if (this.isGuiado) {
+      this.timer = null;
+      alert('TIME IS OVER!! ooooh :(');
+    }
+  }
+
+  paintTimer() {
+    if (this.timerText && this.isGuiado) {
+      this.timerText.text = this.timer.duration.toFixed(0);
+    }
   }
 }
